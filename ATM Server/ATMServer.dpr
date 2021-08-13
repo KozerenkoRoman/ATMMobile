a5c62ef0ae1227b3fc9b626fe7313fca27a2db9f
KozerenkoRoman <kozerenko@gmail.com>
2021-08-13 19:40:31 +0300

    Authentication and SSL

--- ATM Server/ATMServer.dpr
+++ ATM Server/ATMServer.dpr
@@ -7,12 +7,15 @@ uses
   MainForm in 'MainForm.pas' {frmMain},
   Commons in 'Commons.pas',
   MainDM in 'DataModules\MainDM.pas' {dmMain: TDataModule},
-  WebModuleMainU in 'WebModuleMainU.pas' {ATMWebModule: TWebModule},
+  WebModuleMain in 'DataModules\WebModuleMain.pas' {ATMWebModule: TWebModule},
+  Controllers.Users in 'Controllers\Controllers.Users.pas',
+  Controllers.MemoryWebSession in 'Controllers\Controllers.MemoryWebSession.pas',
   Controllers.Base in 'Controllers\Controllers.Base.pas',
   Services.Base in 'Services\Services.Base.pas',
   Entities.AtmIndex in 'Entities\Entities.AtmIndex.pas',
   Controllers.AtmIndex in 'Controllers\Controllers.AtmIndex.pas',
-  Services.AtmIndex in 'Services\Services.AtmIndex.pas';
+  Services.AtmIndex in 'Services\Services.AtmIndex.pas',
+  Authentication in 'Authentication.pas';
 
 {$R *.res}
 
